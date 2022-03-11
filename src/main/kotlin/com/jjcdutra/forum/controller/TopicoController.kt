176a82f2504c28c8c8fe14f3f594344526c196c1
Julio Dutra <jjcdutra@gmail.com>
2022-03-10 21:22:45 -0300

    Implementar atualização de topico

--- src/main/kotlin/com/jjcdutra/forum/controller/TopicoController.kt
+++ src/main/kotlin/com/jjcdutra/forum/controller/TopicoController.kt
@@ -1,5 +1,6 @@
 package com.jjcdutra.forum.controller
 
+import com.jjcdutra.forum.dto.AtualizacaoTopicoForm
 import com.jjcdutra.forum.dto.NovoTopicoForm
 import com.jjcdutra.forum.dto.TopicoView
 import com.jjcdutra.forum.service.TopicoService
@@ -23,7 +24,12 @@ class TopicoController(
     }
 
     @PostMapping
-    fun cadastrar(@RequestBody @Valid dto: NovoTopicoForm) {
-        service.cadastrar(dto)
+    fun cadastrar(@RequestBody @Valid form: NovoTopicoForm) {
+        service.cadastrar(form)
+    }
+
+    @PutMapping
+    fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm) {
+        service.atualizar(form)
     }
 }
--- src/main/kotlin/com/jjcdutra/forum/service/TopicoService.kt
+++ src/main/kotlin/com/jjcdutra/forum/service/TopicoService.kt
@@ -1,5 +1,6 @@
 package com.jjcdutra.forum.service
 
+import com.jjcdutra.forum.dto.AtualizacaoTopicoForm
 import com.jjcdutra.forum.dto.NovoTopicoForm
 import com.jjcdutra.forum.dto.TopicoView
 import com.jjcdutra.forum.mapper.TopicoFormMapper
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Service
 
 @Service
 class TopicoService(
-    var topicos: List<Topico> = ArrayList(),
+    var topicos: MutableList<Topico> = mutableListOf(),
     val topicoViewMapper: TopicoViewMapper,
     val topicoFormMapper: TopicoFormMapper
 ) {
@@ -26,6 +27,23 @@ class TopicoService(
     fun cadastrar(form: NovoTopicoForm) {
         val topico = topicoFormMapper.map(form)
         topico.id = topicos.size.toLong() + 1
-        topicos = topicos.plus(topico)
+        topicos.add(topico)
+    }
+
+    fun atualizar(form: AtualizacaoTopicoForm) {
+        val topico = topicos.first { it.id == form.id }
+        topicos.remove(topico)
+        topicos.add(
+            Topico(
+                id = form.id,
+                titulo = form.titulo,
+                mensagem = form.mensagem,
+                dataCriacao = topico.dataCriacao,
+                curso = topico.curso,
+                autor = topico.autor,
+                status = topico.status,
+                respostas = topico.respostas
+            )
+        )
     }
 }
