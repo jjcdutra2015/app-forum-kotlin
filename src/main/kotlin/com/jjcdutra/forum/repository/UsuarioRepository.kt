582c553fd33866c9052316ac092936d348d9aa1d
Julio Dutra <jjcdutra@gmail.com>
2022-03-18 16:17:39 -0300

    Implementar userdetails

--- src/main/kotlin/com/jjcdutra/forum/repository/UsuarioRepository.kt
+++ src/main/kotlin/com/jjcdutra/forum/repository/UsuarioRepository.kt
@@ -3,4 +3,7 @@ package com.jjcdutra.forum.repository
 import com.jjcdutra.forum.model.Usuario
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UsuarioRepository : JpaRepository<Usuario, Long>
+interface UsuarioRepository : JpaRepository<Usuario, Long> {
+
+    fun findByEmail(username: String?): Usuario?
+}
