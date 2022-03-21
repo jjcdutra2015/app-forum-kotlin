6c2418a5100a319b40abcacbbf010b179ca43aef
Julio Dutra <jjcdutra@gmail.com>
2022-03-21 15:02:54 -0300

    Adicionar JWTAuthenticationFilter

--- src/main/kotlin/com/jjcdutra/forum/config/SecurityConfguration.kt
+++ src/main/kotlin/com/jjcdutra/forum/config/SecurityConfguration.kt
@@ -1,5 +1,6 @@
 package com.jjcdutra.forum.config
 
+import com.jjcdutra.forum.security.JWTAuthenticationFilter
 import com.jjcdutra.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 @EnableWebSecurity
@@ -23,13 +25,14 @@ class SecurityConfguration(
     override fun configure(http: HttpSecurity?) {
         http?.authorizeRequests()?.
 //        antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
-        antMatchers("/login")?.permitAll()?.anyRequest()?.authenticated()?.and()
+        antMatchers("/login")?.permitAll()?.
+        anyRequest()?.authenticated()?.
+        and()
         http?.addFilterBefore(
             JWTLoginFilter(authMananger = authenticationManager(), jwtUtil = jwtUtil),
-            UsernamePasswordAuthenticationFilter().javaClass
-        )
-        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.and()?.formLogin()?.disable()
-            ?.httpBasic()
+                            UsernamePasswordAuthenticationFilter()::class.java)
+        http?.addFilterBefore(JWTAuthenticationFilter(jwtUtil = jwtUtil), OncePerRequestFilter::class.java)
+        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
     }
 
     @Bean
---
+++ src/main/kotlin/com/jjcdutra/forum/security/JWTAuthenticationFilter.kt
@@ -0,0 +1,31 @@
+package com.jjcdutra.forum.security
+
+import com.jjcdutra.forum.config.JWTUtil
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JWTAuthenticationFilter(
+    private val jwtUtil: JWTUtil
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token = request.getHeader("Authorization")
+        val jwt = getTokenDetail(token)
+        if (jwtUtil.isValid(jwt)) {
+            jwtUtil.getAuthentication(jwt)
+        }
+    }
+
+    private fun getTokenDetail(token: String?): String? {
+        return token?.let { jwt ->
+            jwt.startsWith("Bearer ")
+            jwt.substring(7, jwt.length)
+        }
+    }
+}
