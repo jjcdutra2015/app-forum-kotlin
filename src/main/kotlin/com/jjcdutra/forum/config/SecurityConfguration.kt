ee76d4974543d85691afb27caf1e71de4b0dc9d8
Julio Dutra <jjcdutra@gmail.com>
2022-03-21 08:55:21 -0300

    Alterar SecurityConfguration Ãpara trabalhar com Token

--- src/main/kotlin/com/jjcdutra/forum/config/SecurityConfguration.kt
+++ src/main/kotlin/com/jjcdutra/forum/config/SecurityConfguration.kt
@@ -2,6 +2,7 @@ package com.jjcdutra.forum.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -9,23 +10,25 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfguration(
     private val userDetailsService: UserDetailsService
+    private val jwtUtil: JWTUtil
 ) : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity?) {
-        http?.
-        authorizeRequests()?.
-        antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
-        anyRequest()?.authenticated()?.
-        and()?.
-        sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
-        and()?.
-        formLogin()?.disable()?.
-        httpBasic()
+        http?.authorizeRequests()?.
+//        antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
+        antMatchers("/login")?.permitAll()?.anyRequest()?.authenticated()?.and()
+        http?.addFilterBefore(
+            JWTLoginFilter(authMananger = authenticationManager(), jwtUtil = jwtUtil),
+            UsernamePasswordAuthenticationFilter().javaClass
+        )
+        http?.sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.and()?.formLogin()?.disable()
+            ?.httpBasic()
     }
 
     @Bean
