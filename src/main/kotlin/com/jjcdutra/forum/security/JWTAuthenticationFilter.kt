5f9ccae06ec8e2ba3cbf0e37c48cc3f9071aaefa
Julio Dutra <jjcdutra@gmail.com>
2022-03-21 15:27:25 -0300

    Ajustar JWTAuthenticationFilter

--- src/main/kotlin/com/jjcdutra/forum/security/JWTAuthenticationFilter.kt
+++ src/main/kotlin/com/jjcdutra/forum/security/JWTAuthenticationFilter.kt
@@ -1,6 +1,7 @@
 package com.jjcdutra.forum.security
 
 import com.jjcdutra.forum.config.JWTUtil
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
@@ -18,8 +19,10 @@ class JWTAuthenticationFilter(
         val token = request.getHeader("Authorization")
         val jwt = getTokenDetail(token)
         if (jwtUtil.isValid(jwt)) {
-            jwtUtil.getAuthentication(jwt)
+            val authentication = jwtUtil.getAuthentication(jwt)
+            SecurityContextHolder.getContext().authentication = authentication
         }
+        filterChain.doFilter(request, response)
     }
 
     private fun getTokenDetail(token: String?): String? {
